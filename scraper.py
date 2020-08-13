b2f21392439c8c1f385751a72a420c059b48333e
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-08-13 21:30:17 +0530

    added form to index.html and added get_src_tag

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, request
 import requests
+from forms import SearchForm
 from bs4 import BeautifulSoup
 app = Flask(__name__)
 
@@ -10,21 +11,34 @@ headers = {
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Max-Age': '3600',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
+}
 
 
-def validate_url(url):
-    regex = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-    return regex.match(url)
+default_site = "https://unsplash.com/s/photos/web"
+
+
+def get_src_tag(response):
+    soup = BeautifulSoup(response.content, "html5lib")
+    
+
+
+def web_request(website):
+    try:
+        req = requests.get(website, headers=headers)
+        req.raise_for_status()
+    except HTTPError:
+        return []
+    
+    return get_src_tag(req)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    default_site = "https://unsplash.com/s/photos/web"
-    
-    
-    
-    return render_template("index.html")
+    form = SearchForm()
+    if form.validate_on_submit() and web_request(form.website.data):
+            return redirect(url_for("pics.html", src=src))
+        
+    return render_template("index.html", form=form)
 
 
 @app.route('/pictures')
