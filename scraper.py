a60b5d500b37af1b037737a84929ed2a1c013a96
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-08-13 19:51:48 +0530

    created basic app routs and html files

--- scraper.py
+++ scraper.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, request
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
-@app.route('/')
+headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+
+
+def validate_url(url):
+    regex = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    return regex.match(url)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    default_site = "https://unsplash.com/s/photos/web"
+    
+    
+    
+    return render_template("index.html")
+
+
+@app.route('/pictures')
+def pictures():
+    
+    return render_template("pics.html")
 
 
 if __name__ == '__main__':
